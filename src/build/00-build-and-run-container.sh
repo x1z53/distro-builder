db7ed1ce4d7a3e785b51e73915829b001c85d123
dm <dmitry@udalov.online>
2025-03-29 19:36:34 +0600

    fix: test build

--- src/build/00-build-and-run-container.sh
+++ src/build/00-build-and-run-container.sh
@@ -4,15 +4,15 @@ set -e
 
 echo "::group:: ===$(basename "$0")==="
 
-sudo podman build -t iso-builder:latest "${GITHUB_WORKSPACE}/src/source/container"
-sudo podman run -d --privileged \
-  --systemd=always \
-  --cap-add=ALL \
-  --name iso-builder \
-  --tmpfs /run \
-  --tmpfs /run/lock \
-  -v "${GITHUB_WORKSPACE}:/workspace" \
-  -v /sys/fs/cgroup:/sys/fs/cgroup:ro \
-  iso-builder:latest
+#sudo podman build -t iso-builder:latest "${GITHUB_WORKSPACE}/src/source/container"
+#sudo podman run -d --privileged \
+#  --systemd=always \
+#  --cap-add=ALL \
+#  --name iso-builder \
+#  --tmpfs /run \
+#  --tmpfs /run/lock \
+#  -v "${GITHUB_WORKSPACE}:/workspace" \
+#  -v /sys/fs/cgroup:/sys/fs/cgroup:ro \
+#  iso-builder:latest
 
 echo "::endgroup::"
