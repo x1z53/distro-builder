0e2165dfced3967be1ecf43ed5bc83f50d0f5d90
dm <dmitry@udalov.online>
2025-03-29 19:44:45 +0600

    fix:  push iso to yandex storage

--- src/build/01-prepare-image.sh
+++ src/build/01-prepare-image.sh
@@ -4,7 +4,7 @@ set -e
 
 echo "::group:: ===$(basename "$0")==="
 
-#sudo podman exec iso-builder chown -R builder:builder /workspace
-#sudo podman exec iso-builder chmod -R u+w /workspace
+sudo podman exec iso-builder chown -R builder:builder /workspace
+sudo podman exec iso-builder chmod -R u+w /workspace
 
 echo "::endgroup::"
--- src/build/02-build-image.sh
+++ src/build/02-build-image.sh
@@ -4,19 +4,16 @@ set -e
 
 echo "::group:: ===$(basename "$0")==="
 
-mkdir -p "${GITHUB_WORKSPACE}/out"
-echo "Тестовая сборка ISO" | sudo tee ${GITHUB_WORKSPACE}/out/test.txt > /dev/null
-
-#sudo podman exec iso-builder su - builder -c "
-#  cd ~ && \
-#  git clone https://github.com/${GITHUB_REPOSITORY} --branch ${GITHUB_REF##*/} test-builder && \
-#  cd test-builder/src/source && \
-#  git clone https://github.com/altlinux/mkimage-profiles mkimage-profiles && \
-#  cp -rf mkimage/* mkimage-profiles && \
-#  cd mkimage-profiles
-#  make \
-#  IMAGEDIR=\"/workspace/out\" \
-#  BUILDLOG=\"/workspace/out/build.log\" \
-#  regular-gnome-atomic.iso"
+sudo podman exec iso-builder su - builder -c "
+  cd ~ && \
+  git clone https://github.com/${GITHUB_REPOSITORY} --branch ${GITHUB_REF##*/} test-builder && \
+  cd test-builder/src/source && \
+  git clone https://github.com/altlinux/mkimage-profiles mkimage-profiles && \
+  cp -rf mkimage/* mkimage-profiles && \
+  cd mkimage-profiles
+  make \
+  IMAGEDIR=\"/workspace/out\" \
+  BUILDLOG=\"/workspace/out/build.log\" \
+  regular-gnome-atomic.iso"
 
 echo "::endgroup::"
