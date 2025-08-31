cf00d57d32771cc19cb60bba719fbb7235867e92
Vladimir Vaskov <rirusha@altlinux.org>
2025-08-31 14:16:32 +0300

    build: Set mk-p commit

--- src/build/02-build-image.sh
+++ src/build/02-build-image.sh
@@ -9,6 +9,9 @@ sudo podman exec iso-builder su - builder -c "
   git clone https://github.com/${GITHUB_REPOSITORY} --branch ${GITHUB_REF##*/} test-builder && \
   cd test-builder/src/source && \
   git clone -b next https://altlinux.space/mkimage-profiles/mkimage-profiles.git mkimage-profiles && \
+  cd mkimage-profiles && \
+  git checkout bb3408169b8c8dbe6fd7ff941887c6cffaa86998 && \
+  cd ../ && \
   cp -rf mkimage/* mkimage-profiles && \
   cd mkimage-profiles
   make \
