d8471827b502a6909415fda61c57f78481ec4089
x1z53 <x1z53@altlinux.org>
2025-09-04 23:10:20 +0300

    Use next branch

--- src/build/02-build-image.sh
+++ src/build/02-build-image.sh
@@ -8,7 +8,7 @@ sudo podman exec iso-builder su - builder -c "
   cd ~
   git clone https://altlinux.space/x1z53/alt-mobile-mkimage-profiles mkimage-profiles
   cd mkimage-profiles
-  git checkout -b ready-set origin/ready-set
+  git checkout -b next origin/next
   mki-build -- make \
     vm/alt-mobile-phosh-{def,pine}.img \
     BRANCH='sisyphus' ARCH='aarch64' \
