7554be17f5eedffdc348c8b178edd48992c08794
x1z53 <x1z53@altlinux.org>
2025-09-04 23:47:18 +0300

    Add more runtime dependencies

--- src/build/02-build-image.sh
+++ src/build/02-build-image.sh
@@ -10,20 +10,8 @@ sudo podman exec iso-builder su - builder -c "
   cd mkimage-profiles
   git checkout -b next origin/next
   mki-build -- make \
-    vm/alt-mobile-phosh-{def,pine}.img \
-    BRANCH='sisyphus' ARCH='aarch64' \
-    DEBUG=1 STDOUT=1 REPORT=1 CLEAN=1 VM_SAVE_TARBALL=tar
-  mki-build -- make \
-    vm/alt-mobile-phosh-{lt11i,rocknix}.img \
-    BRANCH='sisyphus' ARCH='aarch64' \
-    DEBUG=1 STDOUT=1 REPORT=1 CLEAN=1 VM_SAVE_TARBALL=tar
-  mki-build -- make \
     vm/alt-mobile-phosh-def.img \
     BRANCH='sisyphus' ARCH='x86_64' \
-    DEBUG=1 STDOUT=1 REPORT=1 CLEAN=1
-  mki-build -- make \
-    vm/alt-mobile-phosh-def.img \
-    BRANCH='sisyphus' ARCH='riscv64' \
     DEBUG=1 STDOUT=1 REPORT=1 CLEAN=1
 "
 
