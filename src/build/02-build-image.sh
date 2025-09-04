d88296ae4c1adf9744aa89c9931dbc7521acf4c7
x1z53 <x1z53@altlinux.org>
2025-09-04 22:51:52 +0300

    Add more targets

--- src/build/02-build-image.sh
+++ src/build/02-build-image.sh
@@ -9,8 +9,21 @@ sudo podman exec iso-builder su - builder -c "
   git clone https://altlinux.space/x1z53/alt-mobile-mkimage-profiles mkimage-profiles
   cd mkimage-profiles
   mki-build -- make \
-    vm/alt-mobile-phosh-def.img \
+    vm/alt-mobile-phosh-{def,pine}.img \
+    BRANCH='sisyphus' ARCH='aarch64' \
     DEBUG=1 STDOUT=1 REPORT=1 CLEAN=1 VM_SAVE_TARBALL=tar
+  mki-build -- make \
+    vm/alt-mobile-phosh-{lt11i,rocknix}.img \
+    BRANCH='sisyphus' ARCH='aarch64' \
+    DEBUG=1 STDOUT=1 REPORT=1 CLEAN=1 VM_SAVE_TARBALL=tar
+  mki-build -- make \
+    vm/alt-mobile-phosh-def.img \
+    BRANCH='sisyphus' ARCH='x86_64' \
+    DEBUG=1 STDOUT=1 REPORT=1 CLEAN=1
+  mki-build -- make \
+    vm/alt-mobile-phosh-def.img \
+    BRANCH='sisyphus' ARCH='riscv64' \
+    DEBUG=1 STDOUT=1 REPORT=1 CLEAN=1
 "
 
 echo "::endgroup::"
